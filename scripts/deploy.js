cb9238d478a6668917202caebb558e31b4cae174
Jakub Bogusz <jbogusz03@gmail.com>
2023-01-29 17:29:06 +0100

    Added DCharity smart contract responsible for handeling ether payments on the website

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,10 @@
-const { ethers } = require('hardhat')
+const hre = require('hardhat')
 const fs = require('fs')
 
 async function main() {
-  const contract_name = ''
-  const Contract = await ethers.getContractFactory(contract_name)
-  const contract = await Contract.deploy()
+  const taxFee = 5
+  const Contract = await hre.ethers.getContractFactory('DCharity')
+  const contract = await Contract.deploy(taxFee)
 
   await contract.deployed()
 
@@ -22,3 +22,29 @@ main().catch((error) => {
   console.error(error)
   process.exitCode = 1
 })
+
+
+// const { ethers } = require('hardhat')
+// const fs = require('fs')
+
+// async function main() {
+//   const contract_name = ''
+//   const Contract = await ethers.getContractFactory(contract_name)
+//   const contract = await Contract.deploy()
+
+//   await contract.deployed()
+
+//   const address = JSON.stringify({ address: contract.address }, null, 4)
+//   fs.writeFile('./src/abis/contractAddress.json', address, 'utf8', (err) => {
+//     if (err) {
+//       console.error(err)
+//       return
+//     }
+//     console.log('Deployed contract address', contract.address)
+//   })
+// }
+
+// main().catch((error) => {
+//   console.error(error)
+//   process.exitCode = 1
+// })
